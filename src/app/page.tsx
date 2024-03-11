f6138c4e3b1f36722327b7fb7fd7277f4698d2f7
Aditya Tippanaboena <aditya@Adityas-MacBook-Pro-13.local>
2024-03-10 21:52:23 -0400

    Style Fixes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ export default async function Home() {
   await dbConnect();
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen flex-col items-center justify-between">
       <Selector />
     </main>
   );
